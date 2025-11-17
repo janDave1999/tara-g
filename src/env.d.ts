f8f17f18c69c2f93bedca6fbfae0c859a4e24a80
jdave1999 <jndavezamora@gmail.com>
2025-11-17 12:12:53 +0800

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly SUPABASE_ANON_KEY: string
   readonly SITE_URL: string
   readonly PUBLIC_MAPBOX_TOKEN: string
+  readonly SUPABASE_ANON_KEY: string
   readonly SUPABASE_SERVICE_ROLE_KEY: string,
   readonly PUBLIC_R2_BUCKET: string,
   readonly PUBLIC_S3_CLIENT_API: string,
